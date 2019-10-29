2b1f4386a9d198237a41c647f2f2c295f9ab37ac
tankeluke <45917019+tankeluke@users.noreply.github.com>
2019-10-29 17:15:06 +0800

    wblog_layout

--- src/main/java/com/ws/web/indexController.java
+++ src/main/java/com/ws/web/indexController.java
@@ -2,7 +2,6 @@ package com.ws.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @author Syen
@@ -11,14 +10,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class indexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id,@PathVariable String name){
+    @GetMapping("/index")
+    public String index(){
        // int i=9/0;
       /*  String wblog = null;
         if(wblog == null){
             throw  new NotFoundException("博客不存在");
         }*/
-      System.out.println("------index-----");
+     // System.out.println("------index-----");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        return "blog";
     }
 }
